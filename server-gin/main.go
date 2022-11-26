e6c4dac0f8887c2bd8eecdf820a905cff188b6f8
zhaogongchengsi <zzh1586169624@163.com>
2022-11-26 16:49:17 +0800

    change: gin

--- server-gin/main.go
+++ server-gin/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	//创建实例
+	engine := gin.Default()
+	//处理路由
+	engine.GET("/", func(context *gin.Context) {
+		//以字符串格式返回
+		context.String(200, "这就是一个普通的字符串")
+	})
+	//运行服务器,默认监听8080端口
+	engine.Run()
+
+}
