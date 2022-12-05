11d2fb3a011c3139d5f5454ffb67651e1a6cb479
zhaogongchengsi <zzh1586169624@163.com>
2022-12-05 13:35:01 +0800

    change: style css

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,12 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import "uno.css";
-
-// 单独引入 message 的css样式
-import "@arco-design/web-vue/es/message/style/css.js";
-import "@unocss/reset/tailwind.css";
-import "./style.css";
+import "./style";
 
 import { createAppRouters } from "./routers";
 import { createAppStore } from "./store";
