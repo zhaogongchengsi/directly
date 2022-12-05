b71738fd0635fa05c2f569066cef843a63f78855
zhaogongchengsi <zzh1586169624@163.com>
2022-12-05 17:15:18 +0800

    change: login

--- web/src/views/login/login.vue
+++ web/src/views/login/login.vue
@@ -1,9 +1,26 @@
 <template>
-   <div>
-        登陆
-   </div>
+  <div class="w-full h-screen relative dark:bg-#232324 flex">
+    <div class="flex-1">123</div>
+    <div class="flex-1">123</div>
+    <div class="fixed right-0 top-0 login-mode-card">
+      <Mode></Mode>
+    </div>
+  </div>
 </template>
-<script setup lang='ts'>
-
+<script setup lang="ts">
+import Mode from "@/layouts/mode.vue";
 </script>
-<style lang='scss'></style>
+<style lang="scss" scoped>
+.login-mode-card {
+  border-radius: 0% 100% 0% 100% / 1% 0% 100% 99%;
+  width: 60px;
+  height: 60px;
+  background-color: #08aeea;
+  background-image: linear-gradient(0deg, #08aeea 0%, #2af598 100%);
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  box-sizing: border-box;
+  padding: 10px;
+}
+</style>
