e84d2f4b340d86acbb28cdbab3727b9c8bf2bb5d
zhaogongchengsi <zzh1586169624@163.com>
2022-12-06 15:51:08 +0800

    add: captcha

--- server-nest/src/user/user.controller.ts
+++ server-nest/src/user/user.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, Post, Req } from '@nestjs/common';
 import { ResponseFormat } from 'src/common/response';
 import { UserService } from './user.service';
-import captcha from 'svg-captcha';
+
+import { Request } from 'express';
+
+const CAPTCHA_S_ID = 'captcha';
 
 @Controller('user')
 export class UserController {
@@ -20,6 +23,10 @@ export class UserController {
     return this.response.error(200, '注册失败', new Error('注册失败'));
   }
 
-  @Get('/pictureVerCode')
-  pictureVerificationCode(@Req() request: Request) {}
+  @Get('/captcha')
+  pictureVerificationCode(@Req() request: Request) {
+    const { id, img } = this.userService.createCaptcha();
+    request.session[CAPTCHA_S_ID] = id;
+    return this.response.success({ id, image: img });
+  }
 }
--- server-nest/src/user/user.service.ts
+++ server-nest/src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma.service';
+import * as captcha from 'svg-captcha';
 
 @Injectable()
 export class UserService {
@@ -7,5 +8,19 @@ export class UserService {
 
   async login() {
     return await this.prisma.user.findMany();
+  }
+
+  createCaptcha(size: number = 4) {
+    const picture = captcha.create({
+      size,
+      ignoreChars: 'oi1i', // 忽略相似的字符
+      noise: 3, // 干扰线
+      color: true, // 启用颜色
+    });
+
+    return {
+      id: picture.text,
+      img: picture.data,
+    };
   }
 }
