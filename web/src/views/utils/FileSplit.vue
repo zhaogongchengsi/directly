8dc41241fdbd2deaff3c4e5bf530e4d20b1b8477
zhaogongchengsi <zzh1586169624@163.com>
2022-12-29 15:19:40 +0800

    change: filesplit

--- web/src/views/utils/FileSplit.vue
+++ web/src/views/utils/FileSplit.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="p-3 flex items-center justify-center flex-col">
-    <div class="w-50 h-50 m-15 flex items-center justify-center rounded border border-gray-100 border-dashed cursor-pointer" ref="dropZoneRef">
-      拖拽上传
+    <div
+      :class="[
+        'w-50 h-50 m-15 flex items-center justify-center rounded border dark:border-gray-100 border-gray-900 border-dashed cursor-pointer',
+        { 'border-red-900 border-double bg-gray-300': isOverDropZone },
+      ]"
+      ref="dropZoneRef"
+    >
+      <span>拖拽上传</span>
+      <span>{{ index }}</span>
     </div>
-    <a-button @click="click">切分</a-button>
   </div>
 </template>
 <script setup lang="ts">
@@ -12,6 +18,7 @@ import { ref } from "vue";
 import { useDropZone } from "@vueuse/core";
 
 const dropZoneRef = ref();
+const index = ref(0);
 
 const { isOverDropZone } = useDropZone(dropZoneRef, onDrop);
 
@@ -24,15 +31,11 @@ async function onDrop(files: File[] | null) {
   const filearr = await fileSplit(file, {
     quantity: 3,
     onChanged(indx) {
-      console.log("indx", indx);
+      index.value = indx;
     },
   });
 
   console.log(filearr);
 }
-
-const click = () => {
-  //   const fileWork = fileSplit();
-};
 </script>
 <style lang="scss"></style>
